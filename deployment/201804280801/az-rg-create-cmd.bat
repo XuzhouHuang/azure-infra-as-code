581ffe8a8411f055a3a5258ab1f6ba42f9ebdb86
Allen Kang <allenk@microsoft.com>
2018-05-02 13:13:55 +0800

    remove depllyment folder

--- deployment/201804280801/az-rg-create-cmd.bat
+++
@@ -1,4 +0,0 @@
-﻿az group create --name mc-rg-iac-basic --location ChinaNorth
-az group create --name mc-rg-iac-vm --location ChinaNorth
-az group create --name mc-rg-iac-sf --location ChinaNorth
-az group create --name mc-rg-iac-db --location ChinaNorth
--- deployment/201804280801/az-vnet-create-cmd.bat
+++
@@ -1,6 +0,0 @@
-﻿azbb -c AzureChinaCloud -s c4013028-2728-46b8-acf1-e397840c4344 -l ChinaNorth -g mc-rg-iac-basic -p C:\kangxh\Infra-as-code\deployment\201804280801\arm-vnet-mc-vnet-iac-dev-Param.json --deploy
-azbb -c AzureChinaCloud -s c4013028-2728-46b8-acf1-e397840c4344 -l ChinaNorth -g mc-rg-iac-basic -p C:\kangxh\Infra-as-code\deployment\201804280801\arm-vnet-mc-vnet-iac-uat-Param.json --deploy
-azbb -c AzureChinaCloud -s c4013028-2728-46b8-acf1-e397840c4344 -l ChinaNorth -g mc-rg-iac-basic -p C:\kangxh\Infra-as-code\deployment\201804280801\arm-vnet-mc-vnet-iac-prod-Param.json --deploy
-azbb -c AzureChinaCloud -s c4013028-2728-46b8-acf1-e397840c4344 -l ChinaNorth -g mc-rg-iac-basic -p C:\kangxh\Infra-as-code\deployment\201804280801\arm-vnet-mc-vnet-iac-dev-Param.json --deploy
-azbb -c AzureChinaCloud -s c4013028-2728-46b8-acf1-e397840c4344 -l ChinaNorth -g mc-rg-iac-basic -p C:\kangxh\Infra-as-code\deployment\201804280801\arm-vnet-mc-vnet-iac-uat-Param.json --deploy
-azbb -c AzureChinaCloud -s c4013028-2728-46b8-acf1-e397840c4344 -l ChinaEast -g mc-rg-iac-db -p C:\kangxh\Infra-as-code\deployment\201804280801\arm-vnet-mc-vnet-iac-prod-Param.json --deploy
