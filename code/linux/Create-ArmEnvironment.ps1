75d5f9161dc3f8d13e12dbcbd335fa4028fcfd05
kangxh <kang_xh@hotmail.com>
2018-05-03 11:17:04 +0800

    createenv

--- code/linux/Create-ArmEnvironment.ps1
+++ code/linux/Create-ArmEnvironment.ps1
@@ -4,21 +4,19 @@
 # 3. the excel sheet file. we will copy this file to the deployment folder and rename it as AzureEnv.xlsx
 
  param (
-    [Parameter()][string] $DeployFolder = "C:\kangxh\Infra-as-code\deployment",
-    [Parameter()][string] $PSModule = "C:\kangxh\powershell\allenk-Module-Json.psm1",
-    [Parameter()][string] $AzureFile = "C:\kangxh\Infra-as-code\deployment\poc-daimler.xlsx"
+    [Parameter()][string] $DeployID = "209912310000" # a test folder which will not conflict with current date
+    [Parameter()][string] $DeployFolder = "/var/lib/jenkins/workspace/az-infra-as-code/deployment",
+    [Parameter()][string] $PSModule = "/usr/local/share/powershell/Modules/ImportExcel/4.0.12/allenk-Module-Json.psm1",
+    [Parameter()][string] $AzureFile = "/var/lib/jenkins/workspace/az-infra-as-code/AzureEnv.xlsx"
  )
 
- if ([System.Environment]::OSversion.Platform -match "Win") {$P="\"} else {$P="/"}
-
 # create the deployment folder. the folder should be empty as we create the folder with datetime.
-$targetFolder = get-date -Format yyyyMMddHHmm
-New-Item -ItemType Directory -Force -Path "$DeployFolder$P$targetFolder"
-cd "$DeployFolder$P$targetFolder"
+New-Item -ItemType Directory -Force -Path "$DeployFolder/$DeployID"
+cd "$DeployFolder/$DeployID"
 
 # copy PSModule and AzureFile to the deployment folder
-$moduleFile = ".$P"+"Module.psm1"
-$excelSheet = ".$P"+"AzureEnv.xlsx"
+$moduleFile = "./"+"Module.psm1"
+$excelSheet = "./"+"AzureEnv.xlsx"
 
 Copy-Item -Path $PSModule -Destination $moduleFile
 Copy-Item -Path $AzureFile -Destination $excelSheet
@@ -27,21 +25,13 @@ Copy-Item -Path $AzureFile -Destination $excelSheet
 Import-Module $moduleFile
 $environmentSheet = Import-Excel -Path $excelSheet -WorksheetName Environment -DataOnly
 
-$batchFile = ".$P"+"az-env-create-cmd.bat"
+$batchFile = "./az-env-create-cmd.bat"
 
-if ([System.Environment]::OSversion.Platform -match "Win") {
-    "cd $DeployFolder$P$targetFolder" | Out-File -Encoding utf8 -Append $batchFile
-    "az cloud list --output table" | Out-File -Encoding utf8 -Append $batchFile
-    "az cloud set -n " + $environmentSheet[0].Cloud | Out-File -Encoding utf8 -Append $batchFile
-    "az login --service-principal -u " + $environmentSheet[0].ApplicationID + " -p " + $environmentSheet[0].Certification + " --tenant " + $environmentSheet[0].TenantID  | Out-File -Encoding utf8 -Append $batchFile
-    "az account list --output table" | Out-File -Encoding utf8 -Append $batchFile
-    "az account set -s" + $environmentSheet[0].SubscriptionID | Out-File -Encoding utf8 -Append $batchFile
-} else {
-    "cd $DeployFolder$P$targetFolder"
-    "az cloud list --output table" | Out-File -Encoding utf8 -Append $batchFile
-    "az cloud set -n " + $environmentSheet[1].Cloud | Out-File -Encoding utf8 -Append $batchFile
-    "az login --service-principal -u " + $environmentSheet[1].ApplicationID + " -p " + $environmentSheet[1].Certification + " --tenant " + $environmentSheet[1].TenantID  | Out-File -Encoding utf8 -Append $batchFile
-    "az account list --output table" | Out-File -Encoding utf8 -Append $batchFile
-    "az account set -s" + $environmentSheet[1].SubscriptionID | Out-File -Encoding utf8 -Append $batchFile
-}
+cd "$DeployFolder/$DeployID"
+"az cloud list --output table" | Out-File -Encoding utf8 -Append $batchFile
+"az cloud set -n " + $environmentSheet[1].Cloud | Out-File -Encoding utf8 -Append $batchFile
+"az login --service-principal -u " + $environmentSheet[1].ApplicationID + " -p " + $environmentSheet[1].Certification + " --tenant " + $environmentSheet[1].TenantID  | Out-File -Encoding utf8 -Append $batchFile
+"az account list --output table" | Out-File -Encoding utf8 -Append $batchFile
+"az account set -s" + $environmentSheet[1].SubscriptionID | Out-File -Encoding utf8 -Append $batchFile
+
 
